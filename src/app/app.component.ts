0e153f620dbf7a7119a2a45602424ce86240fb92
samolego <34912839+samolego@users.noreply.github.com>
2022-04-24 19:35:33 +0200

    Firefox window

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import {Component, ElementRef, HostListener} from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,30 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+
   title = 'samolego.github.io';
+
+  constructor(private router: Router, private elRef:ElementRef) {}
+
+  ngOnInit() { }
+
+  get href(): string | undefined {
+    return this.router.url;
+  }
+
+  toggleWindow(windowSelector: string, close: boolean = false) {
+    const window = <HTMLElement> this.elRef.nativeElement.querySelector(windowSelector);
+
+    if (window) {
+      // Animation
+      window.style.display = window.style.display === 'none' ? '' : 'none';
+
+      const button = <HTMLElement> this.elRef.nativeElement.querySelector(windowSelector + "-button");
+      if (button) {
+        button.classList.remove(close ? 'is-dark' : 'is-light');
+        button.classList.add(close ? 'is-light' : 'is-dark');
+      }
+
+    }
+  }
 }
